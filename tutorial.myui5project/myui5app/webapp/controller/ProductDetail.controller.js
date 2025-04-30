465eb4fe9f80fd25e3bfc7ddfa1e1718e30b7fb6
Marco Silva <marco.pucp@gmail.com>
2025-04-30 01:39:30 +0000

    add smart tabl and object pag

--- tutorial.myui5project/myui5app/webapp/controller/ProductDetail.controller.js
+++ tutorial.myui5project/myui5app/webapp/controller/ProductDetail.controller.js
@@ -1,10 +1,11 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast"    
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, MessageToast) {
         "use strict";
 
         return Controller.extend("myui5app.controller.ProductDetail", {
@@ -18,6 +19,9 @@ sap.ui.define([
                 const oView = this.getView();
                 oView.bindElement({
                     path: "/Products(" + iProductId + ")",
+                    parameters: {
+                        expand: "Supplier,Category"
+                    },                    
                     events: {
                         dataRequested: function() {
                             oView.setBusy(true);
@@ -27,6 +31,29 @@ sap.ui.define([
                         }
                     }
                 });
-            }
+            },
+            addToCart: function (oEvent) {
+                MessageToast.show("Added to cart");
+            },
+            
+            markAsFav: function (oEvent) {
+                const oButton = oEvent.getSource();
+                if (oButton.getIcon() === "sap-icon://unfavorite") {
+                    oButton.setIcon("sap-icon://favorite");
+                    MessageToast.show("Added to favorites");
+                    return;
+                }
+            
+                oButton.setIcon("sap-icon://unfavorite");
+                MessageToast.show("Removed from favorites");
+            },
+            trimSuperfluousBytes: function (sVal) { // background info https://blogs.sap.com/2017/02/08/displaying-images-in-sapui5-received-from-the-northwind-odata-service/
+                if (typeof sVal === "string") {
+                    const sTrimmed = sVal.substring(104);
+                    return "data:image/bmp;base64," + sTrimmed;
+                }
+                return sVal;
+            }            
+            
         });
     });
